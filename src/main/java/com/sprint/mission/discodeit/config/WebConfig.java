1b444a317605cc7b5fba16d2973b6f6dc8e3f50e
kangho1870 <ho_gang@naver.com>
2025-05-19 11:05:07 +0900

    feat: WebConfig 정적 리소스 설정, View resolver 설정 추가

--- src/main/java/com/sprint/mission/discodeit/config/WebConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebConfig.java
@@ -1,13 +1,13 @@
 package com.sprint.mission.discodeit.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -15,12 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization");  // 중요: 클라이언트가 접근할 수 있는 헤더
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".html");
     }
-
 }
