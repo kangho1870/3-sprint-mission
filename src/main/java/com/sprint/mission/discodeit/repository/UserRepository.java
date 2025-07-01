fd8ee72ea133537ce8db4602235a9cc57a0d5253
kangho1870 <ho_gang@naver.com>
2025-07-01 12:54:11 +0900

    refactor: UserRepository JOIN FETCH시 profile null일 경우 row 제외하여 @EntityGraph 사용

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,13 +12,10 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    //  @EntityGraph(attributePaths = {"userStatus", "profile"})
-    @Query("SELECT u FROM User u JOIN FETCH u.userStatus JOIN FETCH u.profile")
-    @Override
+    @EntityGraph(attributePaths = {"userStatus", "profile"})
     List<User> findAll();
 
-    @Query("SELECT u FROM User u JOIN FETCH u.userStatus JOIN FETCH u.profile WHERE u.id = :uuid")
-    @Override
+    @EntityGraph(attributePaths = {"userStatus", "profile"})
     Optional<User> findById(UUID uuid);
 
     public Optional<User> findByUsername(String username);
