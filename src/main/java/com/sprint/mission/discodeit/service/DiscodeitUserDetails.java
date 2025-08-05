addda3c05ef856e9813cfa0a0394dbd419929b00
kangho1870 <ho_gang@naver.com>
2025-08-05 10:10:57 +0900

    feat: UserDetailsService Customizer

---
+++ src/main/java/com/sprint/mission/discodeit/service/DiscodeitUserDetails.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class DiscodeitUserDetails implements UserDetails {
+    private final UserDto userDto;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userDto.username();
+    }
+}
